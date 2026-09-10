0f360017dfe0f15a541ac0d7f61478a631df84ef
Dragon-0124 <skseog0841@gmail.com>
2026-09-10 15:41:29 +0900

    Receipt Project v0.2_dev01

---
+++ project/week02/receipt.cpp
@@ -0,0 +1,16 @@
+﻿#include "receipt.h"
+Receipt::Receipt(int receiptNumber) : receiptNumber(receiptNumber), receiptTotal(0)
+{
+}
+Receipt::~Receipt()
+{
+}
+void Receipt::add(int quantity, Beverage beverage)  // use-a
+{
+	receiptTotal += quantity * beverage.getPrice();
+}
+void Receipt::print() const
+{
+	cout << "¿µ¼öÁõ ¹øÈ£: " << receiptNumber << endl;
+	cout << "ÃÑ ±Ý¾×: " << receiptTotal << endl;
+}
