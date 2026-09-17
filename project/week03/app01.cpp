5a03c297c79263e44d03f1366a25d9780c7d6881
Dragon-0124 <skseog0841@gmail.com>
2026-09-17 15:31:40 +0900

    Week03 Polymorphism V0.1_except Virtual Function

---
+++ project/week03/app01.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Pokemon
+{
+public:
+	void attack() const { cout << "Pokemon attacks" << endl; }
+};
+
+class Pikachu : public Pokemon
+{
+public:
+	void attack() const { cout << "1M Volt Thunderbolt" << endl; }
+};
+int main()
+{
+	
+	Pokemon* ptr;
+	
+	ptr = new Pokemon();
+	ptr->attack();
+	delete ptr;
+	ptr = nullptr;
+
+	ptr = new Pikachu();
+	ptr->attack();
+	delete ptr;
+	
+	return 0;
+}
